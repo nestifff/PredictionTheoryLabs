82668d274af88551461fd068525ba639772288bc
Svintilova Anastasiya <anastasiya.svintilova@lemellabs.com>
2022-12-18 15:12:08 +0300

    Lr 1 part 2

--- lr1Common.py
+++ lr1Common.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import lr1_1
+import lr1
 
 
 def showLr1_1():
     startSeries = [13, 16, 20, 18, 21, 24, 25, 22, 28, 29, 25, 32, 33]
-    simpleStep3 = lr1_1.simpleSmoothingOdd(series=startSeries, step=3)
-    simpleStep5 = lr1_1.simpleSmoothingOdd(series=startSeries, step=5)
-    simpleStep7 = lr1_1.simpleSmoothingOdd(series=startSeries, step=7)
+    simpleStep3 = lr1.simpleSmoothingOdd(series=startSeries, step=3)
+    simpleStep5 = lr1.simpleSmoothingOdd(series=startSeries, step=5)
+    simpleStep7 = lr1.simpleSmoothingOdd(series=startSeries, step=7)
 
     seriesDelete13 = startSeries
     seriesDelete13.pop()
-    simpleStep4 = lr1_1.simpleSmoothingEven(seriesDelete13, step=4)
+    simpleStep4 = lr1.simpleSmoothingEven(seriesDelete13, step=4)
 
-    polynomial5Degree = lr1_1.polynomial5DegreeSmoothing(startSeries)
+    polynomial5Degree = lr1.polynomial5DegreeSmoothing(startSeries)
 
     visualizePlot(startSeries)
     # visualizePlot(polynomial5Degree, color="black", skipFirst=2, skipLast=2)
@@ -30,14 +30,14 @@ def showLr1_2():
     sigma = 1.7
 
     normalSeries = np.random.normal(m, sigma, 240)
-    parabolicTrend = lr1_1.getParabolicTrend()
+    parabolicTrend = lr1.getParabolicTrend()
     resultTrend = normalSeries + parabolicTrend
 
     resultTrendOddLength = list(resultTrend.copy())
     resultTrendOddLength.pop()
-    resultTrendSmooth3 = lr1_1.simpleSmoothingOdd(resultTrendOddLength, 3)
-    resultTrendSmooth9 = lr1_1.simpleSmoothingOdd(resultTrendOddLength, 9)
-    resultTrendSmooth12 = lr1_1.simpleSmoothingEven(resultTrend, 12)
+    resultTrendSmooth3 = lr1.simpleSmoothingOdd(resultTrendOddLength, 3)
+    resultTrendSmooth9 = lr1.simpleSmoothingOdd(resultTrendOddLength, 9)
+    resultTrendSmooth12 = lr1.simpleSmoothingEven(resultTrend, 12)
 
     visualizePlot(normalSeries, color="black", marker=".")
     visualizePlot(resultTrend, marker=".")
--- main.py
+++ main.py
@@ -5,26 +5,8 @@
 
 
 import lr1Common
-#
-# polynomial5Degree = lr1.polynomial5DegreeSmoothing(startSeries)
-# lr1.visualizePlot(startSeries)
-# lr1.visualizePlot(polynomial5Degree, color="green", skipFirst=2, skipLast=2)
-# lr1.showAllPlots()
-import lr1_1
-import lr1_2
+
 
 # lr1Common.showLr1_2()
 lr1Common.showLr1_1()
 
-# lr1Common.showSimple357(startSeries)
-
-# seriesDelete13 = startSeries
-# seriesDelete13.pop()
-# simpleStep4 = lr1.simpleSmoothingEven(seriesDelete13, step=4)
-#
-# lr1.visualizePlot(startSeries)
-# lr1.visualizePlot(simpleStep4, color="green")
-# lr1.showAllPlots()
-
-#  Провести сглаживание взвешенной скользящей средней –
-# 5-точечное полиномом 2-й степени,
