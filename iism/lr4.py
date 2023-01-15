d21ec7ab6eca318bafee2004aab5bc4c30578ea9
Svintilova Anastasiya <anastasiya.svintilova@lemellabs.com>
2023-01-16 00:09:38 +0300

    iism lr4 second part

--- iism/lr4.py
+++ iism/lr4.py
@@ -1,6 +1,8 @@
 import math
 import random
 
+import numpy as np
+
 
 def calculateFirstMonteKarlo(n):
     sumI = 0
@@ -12,3 +14,13 @@ def calculateFirstMonteKarlo(n):
         sumI += math.exp(-math.pow(x, 4)) * math.sqrt(1 + math.pow(x, 4)) / \
                 (1.0 / math.sqrt(2 * math.pi) * pow(math.e, -(pow(x, 2) / 2)))
     return sumI / n
+
+
+def calculateSecondMonteKarlo(r1, fi1, r2, fi2, n):
+    z = 0
+    for i in range(n):
+        r = random.uniform(r1, r2)
+        phi = random.uniform(fi1, fi2)
+        z += 1 / (np.power(r * np.cos(phi), 2) + np.power(r * np.sin(phi), 4))
+    p = (fi2 - fi1) * (r2 - r1)
+    return p * z / n
--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import math
-import sys
-
-import lr1Common
 
 # lr1Common.showLr1_2()
 # lr1Common.showLr1_1()
@@ -17,28 +13,29 @@ import lr1Common
 # lr2.pearsonGeometric(n=1000, p=0.5, distr=distr)
 
 # iism.common.plotValuesList(distr2)
-from iism import lr4
-from iism.common import plotValuesList
-from iism.lr1 import linearCongruentMethod, macLarenMarsagliaMethod, pearsonTestUniform, kolmogorovTestUniform
-from iism.lr2 import negBinomialDistribution, geometricDistribution, pearsonNegBinomial, pearsonGeometric
-from iism.lr3 import normalDistribution, laplasDistribution, exponentialDistribution
-from lr2.lr2 import showLr2_1, showLr2_2
+import math
+
+from iism.lr4 import calculateSecondMonteKarlo
+import numpy as np
 from matplotlib import pyplot as plt
-from statistics import mean, variance
 
+#
+#
+# res = 2.87001
+# resExp = calculateSecondMonteKarlo(r1=1, fi1=0, r2=2, fi2=2 * np.pi, n=1000)
+# print(resExp)
+# print(res - resExp)
 
-ansFirst = 2.00005
+ansSecond = 2.87001
 n = 10000
 errors = []
 indices = list(range(n // 100))
 indices = [x * 100 for x in indices]
 for i in range(10, n+1, 100):
-    res = lr4.calculateFirstMonteKarlo(i)
-    error = math.fabs(ansFirst - res) / ansFirst
+    res = calculateSecondMonteKarlo(r1=1, fi1=0, r2=2, fi2=2 * np.pi, n=i)
+    error = math.fabs(ansSecond - res) / ansSecond
     errors.append(error)
 plt.plot(indices, errors, marker=".", color="blue")
 plt.legend()
 plt.grid(True)
 plt.show()
-
-
